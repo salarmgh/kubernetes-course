79520371b90ba99e442fbd674cb2007d906079a5
Salar Moghaddam <salarmgh@gmail.com>
2022-05-13 11:11:25 +0430

    Update

--- session5/statefulset/init.sh
+++ session5/statefulset/init.sh
@@ -1,7 +1,3 @@
-echo "[mysqld]" > /etc/my.cnf.d/server_id.cnf
-echo server-id = $(echo "$(echo $HOSTNAME | cut -d- -f2) + 1" | bc) >> /etc/my.cnf.d/server_id.cnf
-echo "log_bin = /var/lib/mysql/mysql-bin.log" >> /etc/my.cnf.d/repl.cnf
-
 if [[ server-id -eq 1 ]]; then
     mysql -uroot -proot -e "CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
     mysql -uroot -proot -e "GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';"
