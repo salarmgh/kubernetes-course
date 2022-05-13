7403752a90281011d282e624435b7f8dd0a747f6
Salar Moghaddam <salarmgh@gmail.com>
2022-05-13 11:16:01 +0430

    Add server_id

--- session5/statefulset/init.sh
+++ session5/statefulset/init.sh
@@ -1,4 +1,6 @@
-if [[ server-id -eq 1 ]]; then
+server_id=$(echo $HOSTNAME | cut -d- -f2)
+if [[ server_id -eq 0 ]]; then
     mysql -uroot -proot -e "CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
     mysql -uroot -proot -e "GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';"
+    mysql -uroot -proot -e "flush privileges"
 fi
