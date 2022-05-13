494f76207bdc21527003826e42e520b7f37b2fc4
Salar Moghaddam <salarmgh@gmail.com>
2022-05-13 11:29:04 +0430

    Add slave queries

--- session5/statefulset/init.sh
+++ session5/statefulset/init.sh
@@ -4,4 +4,6 @@ if [[ server_id -eq 0 ]]; then
     mysql -uroot -proot -e "CREATE USER 'replica_user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
     mysql -uroot -proot -e "GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';"
     mysql -uroot -proot -e "flush privileges"
+else
+    mysql -uroot -proot -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql-0.mysql', SOURCE_USER='replica_user', SOURCE_PASSWORD='password';"
 fi
